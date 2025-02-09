137d87950dcacbbd9264d7983c7adec8ed6973f8
Prashant Bale <bale.prashant1984@gmail.com>
2025-02-09 18:46:28 +0530

    Update bmt-chatbot-demo.js

--- bmt-chatbot-demo.js
+++ bmt-chatbot-demo.js
@@ -158,7 +158,7 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
